ece5464f03fedebebfd66a55220f5482d5e9cd0e
EC2 Default User <ec2-user@ip-172-31-29-120.us-west-2.compute.internal>
2018-09-12 17:49:58 +0000

    auto deployment to lambda

--- deploy_lambda_package.sh
+++ deploy_lambda_package.sh
@@ -1,7 +1,8 @@
 mkdir mpower2_user_add_package
-pip install synapseclient -t mpower2_user_add_package
-pip install git+https://github.com/larssono/bridgeclient.git -t mpower2_user_add_package
-cp user_add.py main.py .synapseConfig mpower2_user_add_package
+pip3 install --upgrade cython numpy pandas synapseclient -t mpower2_user_add_package
+pip3 install git+https://github.com/larssono/bridgeclient.git -t mpower2_user_add_package
+cp user_add.py .synapseConfig mpower2_user_add_package
 cd mpower2_user_add_package
-#zip -r ../mpower2_user_add_package.zip .
-#aws s3 cp mpower2_user_add_package.zip https://s3-us-west-2.amazonaws.com/mpower2-user-add/mpower2_user_add_package.zip --profile phil-admin
+zip -r ../mpower2_user_add_package.zip .
+aws s3 --profile phil-admin cp ../mpower2_user_add_package.zip s3://mpower2-user-add/mpower2_user_add_package.zip 
+aws lambda update-function-code --function-name mpower2-user-add --s3-bucket mpower2-user-add --s3-key mpower2_user_add_package.zip --profile phil-admin
